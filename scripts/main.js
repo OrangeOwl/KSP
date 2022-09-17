c5b43d459f2f51d9e753df57c21c68b1141646c8
Philip <hibou.devel@gmail.com>
2022-09-17 18:11:32 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,108 @@
+const siamese = ["Jan", "Feb", "Mar"];
+const tabby = ["Apr", "May", "Jun"];
+const brShort = ["Jul", "Aug", "Sep"];
+const maincoon = ["Oct", "Nov", "Dec"];
+
+function ans() {
+  // loading the form div to remove it later
+  const divForm = document.getElementById("form-div");
+  // grabbing the form element and adding an event listener to the submit button
+  document.getElementById("form").addEventListener("submit", function(){
+    // grabbing the values entered into the form
+    const nameValue = document.getElementById("name").value;
+    const monthsValue = document.getElementById("months").value;
+    const drinksValue = document.getElementById("drinks").value;
+    // if name box is empty we don't continue, otherwise grab the values and start the game
+    if (nameValue == ""){
+      alert("You gotta finish!");
+    } else {
+      // remove the form div
+      divForm.remove();
+      // start game passing the variables onto the next function
+      parseData(nameValue, monthsValue, drinksValue);
+    };
+  });
+}
+
+function arcade_game(ship_c, laser_c, player_name) {
+  var can_DOM = document.createElement('canvas');
+  can_DOM.setAttribute('id', 'game-layer');
+  document.body.appendChild(can_DOM);
+  // -------------------------------------------- //
+  var canvas = document.getElementById("game-layer");
+  var context = canvas.getContext("2d");
+
+  context.fillStyle = "grey";
+  context.fillRect(0,0, canvas.width, canvas.height);
+}
+
+function game_start(breed, eye_colour, name) {
+  var ship_c = breed;
+  var laser_c = eye_colour;
+  var player_name = name;
+  // remove all that's left
+  const title = document.getElementById('title');
+  title.remove();
+  const icon = document.getElementById('logo');
+  icon.remove();
+  // create emergency Text
+  document.body.style.backgroundColor = "black";
+  var div = document.createElement('div');
+  div.setAttribute('class', 'night-mode'); // style it to the form div
+  div.innerHTML = "<h1>**EMERGENCY TRANSMISSION**</h1><br><h2>Unfortunately your brief elation must come to an end. The evil space rats have begun an invasion of planet earth, and the super tippity top secret organization known as KSP (Kitty Space Program) have demanded all able-bodied cats must particapte in our global defense.</h2><h2>Say your goodbyes, your cat is needed to save the world.</h2>";
+  document.body.appendChild(div); //append div to body
+  var cont_txt = document.createElement('div');
+  cont_txt.setAttribute('class', 'form');
+  cont_txt.innerHTML = "<button class='SUBMIT'>Good-Bye</button>";
+  document.body.appendChild(cont_txt);
+  cont_txt.addEventListener("click", function(){
+    div.remove();
+    cont_txt.remove();
+    arcade_game(ship_c, laser_c, player_name);
+  });
+}
+
+function parseData(n, m, d) {
+  // determining cat's eye colour
+  var name = n;
+  if (name.length <= 4) {
+    var eye_colour = "blue";
+  } else if (name.length == 5) {
+    var eye_colour = "green";
+  } else {
+    var eye_colour = "yellow";
+  };
+  // determining cats breed
+  if (siamese.includes(m)) {
+    var breed = "Siamese";
+  } else if (tabby.includes(m)) {
+    var breed = "Tabby";
+  } else if (brShort.includes(m)) {
+    var breed = "British ShortHair";
+  } else {
+    var breed = "MainCoon";
+  };
+  // determining cats gender
+  if (d == "Coffee") {
+    var gender = "girl";
+  } else {
+    var gender = "boy";
+  };
+  // Display Data
+  var div = document.createElement('div'); // create a div for text and img
+  div.setAttribute('class', 'form'); // style it to the form div
+  div.innerHTML = "<h1>Congrats on the adoption of your new kitty!</h1><h2>It's a beautiful " + gender + " " + breed +" with " + eye_colour + " coloured eyes </h2><br><img class='cat_icon' src='images/" + breed + "_" + eye_colour + ".png'></img><h2>Say hello to " + name + "</h2><h2>I'm sure you'll become fast friends</h2>";
+  document.body.appendChild(div); //append div to body
+
+  var cont_txt = document.createElement('div');
+  cont_txt.setAttribute('class', 'form');
+  cont_txt.innerHTML = "<button class='SUBMIT'>Continue</button>";
+  document.body.appendChild(cont_txt);
+  cont_txt.addEventListener("click", function(){
+    div.remove();
+    cont_txt.remove();
+    game_start(breed, eye_colour, name);
+  });
+}
+
+ans()
